30692df16fd3283ab6515e68866bf7997c74fb37
NAD Production <admin@nadproduction.com>
2026-06-05 05:43:40 +0200

    refactor: remove Firebase Storage and rules

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-import { getStorage } from 'firebase/storage';
 import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
@@ -18,7 +17,6 @@ const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const storage = getStorage(app);
 export const analytics = getAnalytics(app);
 
 export default app;
